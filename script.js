47b7f8674314202d6a7ab4b4db32d3f1d37239ce
Balagovind-8 <rb.balagovind088@gmail.com>
2025-11-14 12:20:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
 document.getElementById("clickBtn").addEventListener("click", function () {
-  document.getElementById("output").innerText = "Button clicked! App is working!";
+  const output = document.getElementById("output");
+  output.innerText = "Button clicked! App is working beautifully!";
+  output.classList.add("show");
 });
