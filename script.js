d1326aff7c540bdb83fb997a746ed61439baa0aa
Balagovind-8 <rb.balagovind088@gmail.com>
2025-11-14 12:41:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("JS LOADED!");
+console.log("JS connected and ready!");
+
+const btn = document.getElementById("clickBtn");
+const output = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+  output.innerText = "Button clicked! App is working!";
+  output.classList.add("show");
+});
